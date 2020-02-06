1a5a91516b2179c2254b1721b1ff1841df5a1603
leevilaukka <leevi.laukka@edu.keuda.fi>
2020-02-06 13:38:54 +0200

    Toimisko.

---
+++ build/precache-manifest.aae7d7d5122a96a0b6b9b984c5b93a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b665601c64296dc88dc9541cff14ae4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a84d13c081768930f9",
+    "url": "/static/css/main.d76ef549.chunk.css"
+  },
+  {
+    "revision": "0194605fef901f5f13de",
+    "url": "/static/js/2.f6d0c996.chunk.js"
+  },
+  {
+    "revision": "ff9fa00befdea6183a52e83ca846e94a",
+    "url": "/static/js/2.f6d0c996.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3a84d13c081768930f9",
+    "url": "/static/js/main.10c2caa3.chunk.js"
+  },
+  {
+    "revision": "1a4765c77da7114e77e1",
+    "url": "/static/js/runtime-main.13b6ea50.js"
+  }
+]);
