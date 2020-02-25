539f7b246305e94d65a1fec2790ed86ac22553a9
leevilaukka <leevi.laukka@edu.keuda.fi>
2020-02-25 10:45:47 +0200

    Fixes

--- src/components/discord/Login.js
+++ src/components/discord/Login.js
@@ -5,7 +5,7 @@ import Button from "../misc/Button";
 
 const Login = () => {
     return (
-        <Button href="https://foorumiapi.herokuapp.com/discord/login/githubio">
+        <Button href="https://foorumiapi.herokuapp.com/discord/login/">
             <FontAwesomeIcon icon={faDiscord} /> Kirjaudu Discordilla
         </Button>
     );
